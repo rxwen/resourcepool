c77ac35d6482e438f895bdaadc7398038e97562c
Ruixiong Wen <rx.wen218@gmail.com>
2020-04-18 08:28:28 +0800

    udpate unit test code

--- resourcepool_test.go
+++ resourcepool_test.go
@@ -72,6 +72,7 @@ func TestResourcePoolCheckError(t *testing.T) {
 		log.Println("create new resource")
 		return &FakeResource{}, nil
 	}, func(interface{}) error {
+		log.Println("close resource")
 		return nil
 	}, 3, 1)
 
@@ -80,14 +81,86 @@ func TestResourcePoolCheckError(t *testing.T) {
 	con, err := pool.Get()
 	assert.Nil(err)
 	assert.NotNil(con)
+	conNil, err := pool.Get()
+	assert.NotNil(err)
+	assert.Nil(conNil)
 	defer pool.Release(con)
 	fe := errors.New("fake error")
 	count1 := pool.Count()
-	log.Println(pool.Count())
 	assert.Nil(pool.CheckError(con, fe))
 	count2 := pool.Count()
 	assert.NotNil(pool.CheckError(con, fe))
 	count3 := pool.Count()
 	assert.NotEqual(count1, count2)
 	assert.Equal(count3, count2)
+}
+
+func TestResourcePoolReleaseOrder(t *testing.T) {
+	assert := assert.New(t)
+	pool, err := resourcepool.NewResourcePool("fakehost", "9090", func(host, port string) (interface{}, error) {
+		log.Println("create new resource")
+		return &FakeResource{}, nil
+	}, func(interface{}) error {
+		log.Println("close resource")
+		return nil
+	}, 3, 1)
+
+	con1, err := pool.Get()
+	con2, err := pool.Get()
+	con3, err := pool.Get()
+	assert.Nil(err)
+	assert.NotNil(con1)
+	assert.NotNil(con2)
+	assert.NotNil(con3)
+	conNil, err := pool.Get()
+	assert.NotNil(err)
+	assert.Nil(conNil)
+	count1 := pool.Count()
+	assert.Equal(count1, 3)
+	pool.Release(con2)
+	count1 = pool.Count()
+	assert.Equal(count1, 3)
+	err = pool.Release(con2)
+	assert.NotNil(err)
+	log.Println(err)
+	count1 = pool.Count()
+	assert.Equal(count1, 3)
+	con4, err := pool.Get()
+	assert.Nil(err)
+	assert.NotNil(con4)
+	err = pool.Release(con4)
+	assert.Nil(err)
+	err = pool.Release(con1)
+	assert.Nil(err)
+	err = pool.Release(con3)
+	assert.Nil(err)
+	err = pool.Release(con3)
+	assert.NotNil(err)
+
+	idleList := make(chan interface{}, 2)
+	select {
+	case idleList <- con1:
+	default:
+		log.Println("failed to add to chan")
+	}
+	select {
+	case idleList <- con1:
+	default:
+		log.Println("failed to add to chan")
+	}
+	select {
+	case idleList <- con2:
+	default:
+		log.Println("failed to add to chan")
+	}
+	select {
+	case idleList <- con3:
+	default:
+		log.Println("failed to add to chan")
+	}
+	select {
+	case idleList <- con4:
+	default:
+		log.Println("failed to add to chan")
+	}
 }
